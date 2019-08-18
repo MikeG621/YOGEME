b71ecf4a6a589e4433cd5d815d3a0a01cc8edc9c
Mike <mjgaisser@gmail.com>
2019-08-18 10:50:32 -0500

    frontend for Backdrop hook

--- XwaHookDialog.Designer.cs
+++ XwaHookDialog.Designer.cs
@@ -31,46 +31,45 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XwaHookDialog));
 			this.cmdOK = new System.Windows.Forms.Button();
 			this.cmdCancel = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
+			this.grpBackdrops = new System.Windows.Forms.GroupBox();
+			this.chkBackdrops = new System.Windows.Forms.CheckBox();
+			this.lstBackdrops = new System.Windows.Forms.ListBox();
+			this.cmdAddBD = new System.Windows.Forms.Button();
+			this.cmdRemoveBD = new System.Windows.Forms.Button();
+			this.opnBackdrop = new System.Windows.Forms.OpenFileDialog();
+			this.grpBackdrops.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// cmdOK
 			// 
 			this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.cmdOK.Location = new System.Drawing.Point(166, 272);
+			this.cmdOK.Location = new System.Drawing.Point(180, 462);
 			this.cmdOK.Name = "cmdOK";
 			this.cmdOK.Size = new System.Drawing.Size(75, 23);
 			this.cmdOK.TabIndex = 0;
 			this.cmdOK.Text = "&OK";
 			this.cmdOK.UseVisualStyleBackColor = true;
+			this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
 			// 
 			// cmdCancel
 			// 
 			this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cmdCancel.Location = new System.Drawing.Point(266, 272);
+			this.cmdCancel.Location = new System.Drawing.Point(465, 432);
 			this.cmdCancel.Name = "cmdCancel";
 			this.cmdCancel.Size = new System.Drawing.Size(75, 23);
 			this.cmdCancel.TabIndex = 0;
 			this.cmdCancel.Text = "&Cancel";
 			this.cmdCancel.UseVisualStyleBackColor = true;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(58, 13);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Backdrops";
+			this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 54);
+			this.label2.Location = new System.Drawing.Point(10, 211);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(60, 13);
 			this.label2.TabIndex = 1;
@@ -79,7 +78,7 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 97);
+			this.label3.Location = new System.Drawing.Point(12, 256);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(47, 13);
 			this.label3.TabIndex = 1;
@@ -88,7 +87,7 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(12, 134);
+			this.label4.Location = new System.Drawing.Point(12, 294);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(81, 13);
 			this.label4.TabIndex = 1;
@@ -97,11 +96,70 @@
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(12, 179);
+			this.label5.Location = new System.Drawing.Point(12, 340);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(74, 13);
 			this.label5.TabIndex = 1;
 			this.label5.Text = "Engine Sound";
+			// 
+			// grpBackdrops
+			// 
+			this.grpBackdrops.Controls.Add(this.cmdRemoveBD);
+			this.grpBackdrops.Controls.Add(this.cmdAddBD);
+			this.grpBackdrops.Controls.Add(this.lstBackdrops);
+			this.grpBackdrops.Controls.Add(this.chkBackdrops);
+			this.grpBackdrops.Location = new System.Drawing.Point(12, 12);
+			this.grpBackdrops.Name = "grpBackdrops";
+			this.grpBackdrops.Size = new System.Drawing.Size(289, 96);
+			this.grpBackdrops.TabIndex = 2;
+			this.grpBackdrops.TabStop = false;
+			this.grpBackdrops.Text = "Backdrops";
+			// 
+			// chkBackdrops
+			// 
+			this.chkBackdrops.AutoSize = true;
+			this.chkBackdrops.Location = new System.Drawing.Point(6, 19);
+			this.chkBackdrops.Name = "chkBackdrops";
+			this.chkBackdrops.Size = new System.Drawing.Size(99, 17);
+			this.chkBackdrops.TabIndex = 0;
+			this.chkBackdrops.Text = "Use Backdrops";
+			this.chkBackdrops.UseVisualStyleBackColor = true;
+			this.chkBackdrops.CheckedChanged += new System.EventHandler(this.chkBackdrops_CheckedChanged);
+			// 
+			// lstBackdrops
+			// 
+			this.lstBackdrops.Enabled = false;
+			this.lstBackdrops.FormattingEnabled = true;
+			this.lstBackdrops.Location = new System.Drawing.Point(6, 42);
+			this.lstBackdrops.Name = "lstBackdrops";
+			this.lstBackdrops.Size = new System.Drawing.Size(141, 43);
+			this.lstBackdrops.TabIndex = 1;
+			// 
+			// cmdAddBD
+			// 
+			this.cmdAddBD.Enabled = false;
+			this.cmdAddBD.Location = new System.Drawing.Point(153, 42);
+			this.cmdAddBD.Name = "cmdAddBD";
+			this.cmdAddBD.Size = new System.Drawing.Size(60, 23);
+			this.cmdAddBD.TabIndex = 2;
+			this.cmdAddBD.Text = "&Add";
+			this.cmdAddBD.UseVisualStyleBackColor = true;
+			this.cmdAddBD.Click += new System.EventHandler(this.cmdAddBD_Click);
+			// 
+			// cmdRemoveBD
+			// 
+			this.cmdRemoveBD.Enabled = false;
+			this.cmdRemoveBD.Location = new System.Drawing.Point(219, 42);
+			this.cmdRemoveBD.Name = "cmdRemoveBD";
+			this.cmdRemoveBD.Size = new System.Drawing.Size(60, 23);
+			this.cmdRemoveBD.TabIndex = 2;
+			this.cmdRemoveBD.Text = "&Remove";
+			this.cmdRemoveBD.UseVisualStyleBackColor = true;
+			this.cmdRemoveBD.Click += new System.EventHandler(this.cmdRemoveBD_Click);
+			// 
+			// opnBackdrop
+			// 
+			this.opnBackdrop.Filter = "Dat Files|*.dat";
 			// 
 			// XwaHookDialog
 			// 
@@ -109,12 +167,12 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.cmdCancel;
-			this.ClientSize = new System.Drawing.Size(462, 321);
+			this.ClientSize = new System.Drawing.Size(891, 553);
+			this.Controls.Add(this.grpBackdrops);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
 			this.Controls.Add(this.cmdCancel);
 			this.Controls.Add(this.cmdOK);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -122,6 +180,8 @@
 			this.MinimizeBox = false;
 			this.Name = "XwaHookDialog";
 			this.Text = "Mission Hook Settings";
+			this.grpBackdrops.ResumeLayout(false);
+			this.grpBackdrops.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -131,10 +191,15 @@
 
 		private System.Windows.Forms.Button cmdOK;
 		private System.Windows.Forms.Button cmdCancel;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.GroupBox grpBackdrops;
+		private System.Windows.Forms.Button cmdRemoveBD;
+		private System.Windows.Forms.Button cmdAddBD;
+		private System.Windows.Forms.ListBox lstBackdrops;
+		private System.Windows.Forms.CheckBox chkBackdrops;
+		private System.Windows.Forms.OpenFileDialog opnBackdrop;
 	}
 }
--- XwaHookDialog.cs
+++ XwaHookDialog.cs
@@ -5,15 +5,85 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Idmr.Yogeme
 {
 	public partial class XwaHookDialog : Form
 	{
-		public XwaHookDialog()
+		// this is going to be setup to read from the individual TXT files, but always write to Mission.ini
+		string _mission;
+		string _fileName = "";
+		string _bdFile = "";
+		string _soundFile = "";
+		string _objFile = "";
+		string _missionTxtFile = "";
+		string _hangarObjectsFile = "";
+		string _hangarCameraFile = "";
+		string _famHangarCameraFile = "";
+		string _hangarMapFile = "";
+		string _famHangarMapFile = "";
+		string _installDirectory = "";
+		string _res = "\\RESDATA\\";
+		string _wave = "\\Wave\\";
+		string _fm = "\\FlightModels\\";
+
+		public XwaHookDialog(string missionFile)
 		{
 			InitializeComponent();
+			_mission = Idmr.Common.StringFunctions.GetFileName(missionFile, false);
+			_fileName = missionFile.Replace(".tie", ".ini");
+			Settings s = new Settings();
+			if (s.XwaInstalled)
+			{
+				_installDirectory = s.XwaPath;
+				grpBackdrops.Enabled = File.Exists(_installDirectory + "\\Hook_Backdrops.dll");
+
+				_bdFile = checkFile("_Resdata.txt");
+				_soundFile = checkFile("_Sounds.txt");
+				_objFile = checkFile("_Objects.txt");
+				_missionTxtFile = checkFile(".txt");
+				_hangarObjectsFile = checkFile("_HangarObjects.txt");
+				_hangarCameraFile = checkFile("_HangarCamera.txt");
+				_famHangarCameraFile = checkFile("_FamHangarCamera.txt");
+				_hangarMapFile = checkFile("_HamgarMap.txt");
+				_famHangarMapFile = checkFile("_FamHangarMap.txt");
+			}
+		}
+
+		string checkFile(string extension)
+		{
+			if (File.Exists(_installDirectory + "\\Missions\\" + _mission + extension)) return _installDirectory + "\\Missions\\" + _mission + extension;
+			return "";
+		}
+
+		#region Backdrops
+		private void chkBackdrops_CheckedChanged(object sender, EventArgs e)
+		{
+			lstBackdrops.Enabled = chkBackdrops.Checked;
+			cmdAddBD.Enabled = chkBackdrops.Checked;
+			cmdRemoveBD.Enabled = chkBackdrops.Checked;
+		}
+
+		private void cmdAddBD_Click(object sender, EventArgs e)
+		{
+
+		}
+		private void cmdRemoveBD_Click(object sender, EventArgs e)
+		{
+			if (lstBackdrops.SelectedIndex == -1) return;
+			lstBackdrops.Items.RemoveAt(lstBackdrops.SelectedIndex);
+		}
+		#endregion Backdrops
+
+		private void cmdCancel_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+		private void cmdOK_Click(object sender, EventArgs e)
+		{
+			Close();
 		}
 	}
 }
